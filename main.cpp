6c01cba5368a8247cdc74b6a89c722af124f5329
Yuno-projekt <maciek.kopczynski0@gmail.com>
2026-01-30 12:46:56 +0100

    Fix Polish language typos in menu and rules

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@ void showMenu() {
 cout << "\n========== SNAKE GAME ==========\n";
 cout << "1. Nowa gra\n";
 cout << "2. Zasady\n";
-cout << "3. Wyjście\n";
+cout << "3. Wyjscie\n";
 cout << "================================\n";
-cout << "Wybór: ";
+cout << "Akcja numer: ";
 }
 void showRules() {
 cout << "\n=== ZASADY GRY ===\n";
-cout << "- Steruj wężem używając klawiszy W, A, S, D\n";
-cout << "- Zbieraj jedzenie (*) aby rosnąć i zdobywać punkty\n";
-cout << "- Unikaj ścian i własnego ciała\n";
-cout << "- Wciśnij Q aby zakończyć grę\n";
-cout << "\nWciśnij Enter aby wrócić...";
+cout << "- Steruj wezem uzywając klawiszy W, A, S, D\n";
+cout << "- Zbieraj jedzenie (*) aby rosnac i zdobywac punkty\n";
+cout << "- Unikaj kolizji z scianami i wlasnym cialem\n";
+cout << "- Wcisnij Q aby zakonczyc gre\n";
+cout << "\nWcisnij Enter aby wrocic...";
 cin.ignore();
 cin.get();
 }
@@ -37,7 +37,7 @@ case 3:
 cout << "Do zobaczenia!\n";
 break;
 default:
-cout << "Nieprawidłowy wybór!\n";
+cout << "Nieprawidlowy wybor!\n";
 }
 } while (choice != 3);
 return 0;
